71edda1969999c58319352eaca07d8cbf321e0dd
Leafy2001 <51094578+Leafy2001@users.noreply.github.com>
2020-06-18 20:16:15 +0530

    b

---
+++ build/precache-manifest.e4fce9ab8d2127791a535e772cbc4bd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71de085509316f6ad102ce89e3a4086c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20af60d1e73dd1a4b525",
+    "url": "/static/css/main.8a8c1ef9.chunk.css"
+  },
+  {
+    "revision": "fe167b19e50af53ad139",
+    "url": "/static/js/2.ad5afae5.chunk.js"
+  },
+  {
+    "revision": "20af60d1e73dd1a4b525",
+    "url": "/static/js/main.eef019a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "13669363bfeba2935ac96e0bbda2456c",
+    "url": "/static/media/dp.13669363.jpg"
+  }
+]);
